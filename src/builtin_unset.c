c55d3fe310a6853947a2ae85f1c4f6f1868e76ee
Олег Комисаренко <oleg.o.komisarenko@gmail.com>
2021-12-12 22:36:54 +0300

    checked compatibility with linked list

--- src/builtin_unset.c
+++ src/builtin_unset.c
@@ -20,32 +20,16 @@ int	build_unset(char **str, t_struct *p)
 	i = 0;
 	if (unset_errors(str))
 		return (-1);
-	// while (p->arr_env[i])
-	// {
-	// 	stop_position = ft_strchr(p->arr_env[i], '=');
-	// 	if (stop_position)
-	// 	{
-	// 		*stop_position = '\0';
-	// 		compare = ft_strdup(p->arr_env[i]);
-	// 		if (find_str(str[1], compare))
-	// 		{	
-	// 			ft_putstr_fd(compare, 1);
-	// 			ft_putstr_fd("\n", 1);
-	// 		}
-	// 		free(compare);
-	// 	}
-	// 	i++;
-	// }
 	tmp = p->my_env;
 	if (tmp != NULL)
 	{
 		while (tmp->next != NULL)
 		{
-			stop_position = ft_strchr(p->my_env->var, '=');
+			stop_position = ft_strchr(tmp->var, '=');
 			if (stop_position)
 			{
 				*stop_position = '\0';
-				compare = ft_strdup(p->my_env->var);
+				compare = ft_strdup(tmp->var);
 				if (find_str(str[1], compare))
 				{	
 					ft_putstr_fd(compare, 1);
