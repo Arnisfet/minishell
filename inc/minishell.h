5fc26d7972dea9f4021ce28e2d01fd5845f9594c
Олег Комисаренко <oleg.o.komisarenko@gmail.com>
2021-12-13 21:36:44 +0300

    s

--- inc/minishell.h
+++ inc/minishell.h
@@ -36,7 +36,6 @@ void	init_env(char **env, t_struct *p);
 void	display_message(t_struct *p);
 int		build_cd(char **str, t_struct *p);
 int		input_is_empty(char *check);
-char	*get_env_var(char *str, t_struct *p);
 char	*build_pwd(char **str);
 int		find_str(const char *s1,const char *s2);
 int		build_export(char **str, t_struct *p);
@@ -45,5 +44,6 @@ void	init_env_list(t_struct *p, char *content);
 int		parse_cmd(char **str, t_struct *p);
 int		builtin_echo(char **str, t_struct *p);
 int		builtin_env(t_struct *p);
+char	*get_env_var(char *str, t_struct *p);
 
 #endif
